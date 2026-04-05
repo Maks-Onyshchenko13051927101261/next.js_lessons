0f877aaef07e03421c76a47840c5068d4ee3dbe2
Maks Onyshchenko <onyshchenkomaksymm@gmail.com>
2026-04-05 07:29:33 +0000

    feat: add CommentPage

--- src/app/(public)/comments/[id]/page.tsx
+++ src/app/(public)/comments/[id]/page.tsx
@@ -4,15 +4,24 @@ import { FC } from "react";
 
 type CommentPropsType = {
     params: {id:string},
-}
+};
 
 export const generateMetadata = async ({params}: CommentPropsType): Promise<Metadata> => {
     const {id} = await params;
-    const comment = allEndpoint.comments.getById(id);
+    const comment = await allEndpoint.comments.getById(id);
     return {
-        title: ``
-    }
-}
-const CommentIdPage:FC<CommentPropsType> = () => {};
+        title: `${id} - ${comment.name}`,
+    };
+};
+const CommentIdPage:FC<CommentPropsType> = async ({params}) => {
+    const {id} = await params;
+    const comment = await allEndpoint.comments.getById(id);
+    return (
+        <div>
+            <h6>title: {comment.id} - {comment.email}</h6>
+            <p>{comment.body}</p>
+        </div>
+    );
+};
 
 export default CommentIdPage;
