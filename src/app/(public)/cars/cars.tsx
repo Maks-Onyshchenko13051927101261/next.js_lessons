cd9566848698da12f6e3ad8f34c344bf6359a15a
maks <onyshchenkomaksymm@gmail.com>
2026-07-25 07:09:28 +0300

    feat: Cars page

---
+++ src/app/(public)/cars/cars.tsx
@@ -0,0 +1,11 @@
+import { CarsComponent } from "@/components/cars/CarsComponent";
+
+const CarsPage = () => {
+    return (
+        <div>
+            <CarsComponent/>
+        </div>
+    );
+};
+
+export default CarsPage;
