69f2c3754ec4839a4f7adc7140ee071b47fe5916
maks <onyshchenkomaksymm@gmail.com>
2026-07-30 19:25:46 +0300

    feat: carValidator

--- src/validator/carValidator.tsx
+++ src/validator/carValidator.tsx
@@ -1,3 +1,22 @@
 import Joi from "joi";
 
-export const carValidator = Joi.object({});
+export const carValidator = Joi.object({
+    brand: Joi
+    .string()
+    .pattern(new RegExp("^[a-zA-Za-яА-яёЁіІїЇєЄҐґ]{1,20}$"))
+    .messages({ "string.pattern.base": "Invalid brand name" }),
+    price: Joi
+    .number()
+    .min(0)
+    .max(1000000)
+    .messages({ 
+        "number.min": "Price must be a positive number",
+        "number.max": "Price limit is 1000000" }),
+    year: Joi
+    .number()
+    .min(1886)
+    .max(2026)
+    .messages({ 
+        "number.min": "Year must be 1986 or later",
+        "number.max": "Year must be 2026 or earlier" }),
+});
